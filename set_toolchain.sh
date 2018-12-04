b1b9169f69a35aca6ec3750346b67083be437c24
xlloss <xlloss@gmail.com>
2018-12-04 17:31:07 +0800

    add set_toolchain.sh for initial build env

---
+++ set_toolchain.sh
@@ -0,0 +1,21 @@
+export AArch=64
+
+export CROSS_COMPILE=aarch64-elf-
+export LSI=H3
+#export LSI=M3
+#export EMMC=ENABLE
+export EMMC=DISABLE
+
+export SERIAL_FLASH=ENABLE
+
+export USB=DISABLE
+
+#export BOARD=ULCB
+export BOARD=SALVATOR
+export PATH=$PATH:/home/slash/project/renesas/Gen3/gen3_bootlader/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin/
+
+
+
+export ARCH=aarch64
+export PLAT=rcar
+
